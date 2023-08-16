0fc83e543e2819b2224188385379972db153de4f
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-08-16 12:37:40 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {
+  AddJobs,
+  AllJobs,
+  Error,
+  Home,
+  Landing,
+  Login,
+  Profile,
+} from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="">Hunt Trackr</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/all-jobs" element={<AllJobs />} />
+        <Route path="/add-jobs" element={<AddJobs />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import AddJobs from "./AddJobs";
+import AllJobs from "./AllJobs";
+import Error from "./Error";
+import Home from "./Home";
+import Landing from "./Landing";
+import Login from "./Login";
+import Profile from "./Profile";
+
+export { AddJobs, AllJobs, Error, Home, Landing, Login, Profile };
