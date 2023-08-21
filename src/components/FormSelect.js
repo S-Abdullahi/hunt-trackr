7e5883251c45c24bd8c9c0123b1acf7154de7270
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-08-21 22:36:42 +0100

    add job screen conversion

--- src/components/FormSelect.js
+++ src/components/FormSelect.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const FormSelect = () => {
+const FormSelect = ({ selectOptions, onChange, name, value }) => {
   return (
-    <div>FormSelect</div>
-  )
-}
+    <div className="w-[100%] ">
+      <div className="mb-1 text-gray-600 text-sm">{name}</div>
+      <select
+        name={name}
+        onChange={onChange}
+        className="mb-5 px-2 h-10 rounded-sm border focus:border-[#fd5732] focus:outline-none text-[gray] w-full cursor-pointer"
+      >
+        {selectOptions.map((item) => {
+          return (
+            <option className="" value={value}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
 
-export default FormSelect
+export default FormSelect;
--- src/pages/AddJobs.js
+++ src/pages/AddJobs.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import FormRow from "../components/FormRow";
+import FormSelect from "../components/FormSelect";
+
+const status = ["Approved", "Pending", "Declined"];
+const jobType = ["Internship", "Full-time", "Part time", "remote"];
 
 const AddJobs = () => {
   return (
-    <div>AddJobs</div>
-  )
-}
+    <div className="flex justify-center items-center pt-10">
+      <div className="bg-white p-4 w-[80%] sm:w-[60%] md:w-1/2 shadow-md">
+        <p className="text-center text-lg mb-6">Add Job</p>
+        <form className="flex flex-col">
+          <FormRow name="Position" value="" />
+          <FormRow name="Company" value="" />
+          <FormRow name="Job Location" value="" />
+          <div className="flex justify-between gap-4">
+            <FormSelect name="status" selectOptions={status} />
+            <FormSelect name="job type" selectOptions={jobType} />
+          </div>
+          <div className="flex justify-between gap-4">
+            <button className="border rounded-sm py-2 bg-transparent w-full">Clear</button>
+            <button
+              type="submit"
+              className="rounded-sm text-white bg-[#fd5732] py-2 w-full"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default AddJobs
+export default AddJobs;
