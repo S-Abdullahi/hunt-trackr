aff0bfbe0ed94be1205452f4071c4bd80aa5e98a
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-08-30 15:10:24 +0100

    grid view enhancement

--- src/components/Grid/GridView.js
+++ src/components/Grid/GridView.js
@@ -1,12 +1,28 @@
 import React from "react";
 import GridCard from "./GridCard";
+import Pagination from "../Pagination";
+import { useSelector } from "react-redux";
+import Loader from "../loader";
 
 const GridView = ({ jobs }) => {
+  const { numberOfPages, isLoading } = useSelector((store) => store.allJobs);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-4 mt-5">
-      {jobs?.map((job) => {
-        return <GridCard {...job} />;
-      })}
+    <div>
+      {isLoading ? (
+        <div className="flex justify-center mt-6">
+          <Loader />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-4 mt-5">
+          {jobs?.map((job) => {
+            return <GridCard {...job} />;
+          })}
+        </div>
+      )}
+
+      <div className="flex justify-center">
+        {numberOfPages > 1 && <Pagination />}
+      </div>
     </div>
   );
 };
