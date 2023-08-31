d9f992ff393bd0096d96c90b929cf5e9538b0c9e
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-08-31 11:49:29 +0100

    side bar modification

--- src/components/MobileSideBar.js
+++ src/components/MobileSideBar.js
@@ -8,6 +8,8 @@ import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { BiMenu } from "react-icons/bi";
 import { BsPersonCircle } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { logOut } from "../Features/users/UserSlice";
 
 const sideMenuItem = [
   {
@@ -33,37 +35,52 @@ const sideMenuItem = [
 ];
 
 const MobileSideBar = ({ open, setOpenSideBar }) => {
+  const dispatch = useDispatch();
+  const sideTransition = "transition-all duration-[300ms] ease-linear";
   return (
-    <div
-      className={`flex flex-col justify-between pt-2 z-50 border-t-[1px] md:hidden fixed h-[calc(100%-48px)] w-full ${
-        open ? "m-0" : "-m-[100%]"
-      }`}
-    >
-      <div>
-        <ul className="mt-16">
-          {sideMenuItem.map((item) => {
-            const { name, url, icon } = item;
-            return (
-              <li
-                key={name}
-                className="text-sm mb-4 hover:bg-[#fd5732] hover:text-white pl-4 py-1 hover:ease-in-out"
-                onClick={() => setOpenSideBar(false)}
-              >
-                <Link to={url} className="flex items-center gap-2">
-                  {icon} {name}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <Link
-        to="/login"
-        className="flex items-center justify-center pb-6 text-red-400 gap-2"
+    <div className="relative">
+      <div
+        className={`flex flex-col justify-between bg-white pt-2 z-50 border-t-[1px] md:hidden fixed h-[calc(100%-48px)] ${
+          open ? `m-0 w-1/2 ${sideTransition}` : `-ml-[300px] ${sideTransition}`
+        }`}
       >
-        <MdLogout /> Logout
-      </Link>
+        <div>
+          <ul className="mt-16">
+            {sideMenuItem.map((item) => {
+              const { name, url, icon } = item;
+              return (
+                <li
+                  key={name}
+                  className="text-sm mb-4 hover:bg-[#fd5732] hover:text-white pl-4 py-1 hover:ease-in-out"
+                  onClick={() => setOpenSideBar(false)}
+                >
+                  <Link to={url} className="flex items-center gap-2">
+                    {icon} {name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <Link
+          to="/login"
+          className="flex items-center justify-center pb-6 text-red-400 gap-2"
+          onClick={() => dispatch(logOut())}
+        >
+          <MdLogout /> Logout
+        </Link>
+      </div>
+      {/* side bar transparent background */}
+
+      <div
+        className={`fixed h-[calc(100%-48px)] w-full bg-black z-30 ${
+          open
+            ? `m-0 w-full opacity-70 ${sideTransition}`
+            : "opacity-0 ${sideTransition}"
+        }`}
+        onClick={() => setOpenSideBar(false)}
+      ></div>
     </div>
   );
 };
