c5c5bb58fbd3d32528a208ec7397cf574e5cfd02
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-08-31 09:53:39 +0100

    error page styling

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import errorImage from "../assets/error.svg";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
-    <div>Error</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen gap-5">
+      <img src={errorImage} alt="error image"  className="w-[30%]"/>
+      <button onClick={() => navigate("/login")} className="bg-[#fd5732] px-2 py-1 rounded text-white hover:bg-[#fd5732d1]">Return</button>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
